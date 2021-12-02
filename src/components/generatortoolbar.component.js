817da43e00ecaa3f853a9344c0f553344d89ac98
AlexIchenskiy <aichenskiy@gmail.com>
2021-12-02 20:43:57 +0100

    feat: mobile generate button (Link wrapped Button)

--- src/components/generatortoolbar.component.js
+++ src/components/generatortoolbar.component.js
@@ -1,11 +1,17 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
-import { Card } from 'ui-neumorphism';
+import { Button, Card } from 'ui-neumorphism';
 
 function GeneratorToolbar() {
 	return (
 		<Card className='Generator-toolbar' style={{borderRadius: '0'}}>
 			<h2 className='Generator-toolbar-subtitle'>Press spacebar to quickly generate a new palette!</h2>
+			<Link to="/polichroma/generator" className='Link-nostyle Link-toolbar-generate'>
+				<Button className='Generate-toolbar-generate'>
+					<p style={{fontSize: 24 + 'px', fontWeight: 500}}>Generate!</p>
+				</Button>
+			</Link>
 			<div className='Generator-toolbar-tools'>
 			</div>
 		</Card>
