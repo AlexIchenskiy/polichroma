ba45ffc84eec264c40ff04556667304c0a5772a9
Alex Ichenskiy <aichenskiy@gmail.com>
2022-03-26 08:57:12 +0100

    feat: toolset copy to clipboard

--- src/components/generatortoolset.component.js
+++ src/components/generatortoolset.component.js
@@ -1,14 +1,22 @@
 import React from 'react';
 
+import { FiCopy } from 'react-icons/fi';
+
 function GeneratorToolset(props) {
     let styles = {
         color: props.textColor,
     };
 
+    function copyText() {
+        navigator.clipboard.writeText(props.color);
+    }
+
 	return (
 		<div className = 'Generator-palette-toolset'>
             <div className = 'Generator-palette-toolset-tools'>
-
+                <div className = 'Generator-palette-toolset-icon' onClick={() => {copyText()}}>
+                    <FiCopy size = {'32px'} color = {props.textColor}></FiCopy>
+                </div>
             </div>
             <div className = 'Generator-palette-toolset-colorname'>
                 <h1 style = {styles} >
